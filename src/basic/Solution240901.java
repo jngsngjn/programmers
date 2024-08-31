9051ead33e8095bcd31c55d9c7445cb0fb5e23b5
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-01 00:23:20 +0900

    240901

---
+++ src/basic/Solution240901.java
@@ -0,0 +1,21 @@
+package basic;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181922
+ */
+class Solution240901 {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (j % (queries[i][2]) == 0) {
+                    arr[j]++;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
