1b6646a66f93733093edb3178d7c8297a9b1d66e
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-01 09:54:35 +0900

    solution04

---
+++ src/solutions/Solution04.java
@@ -0,0 +1,29 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181919
+ */
+class Solution04 {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(n);
+
+        while (true) {
+            if (n == 1) {
+                break;
+            }
+
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            list.add(n);
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
