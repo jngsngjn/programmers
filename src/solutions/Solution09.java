356ec3bb6f5f259ef8934ceb4c68864db59b64e0
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-02 11:52:15 +0900

    solution09

---
+++ src/solutions/Solution09.java
@@ -0,0 +1,15 @@
+package solutions;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181914
+ */
+class Solution09 {
+    public int solution(String number) {
+        char[] charArray = number.toCharArray();
+        int sum = 0;
+        for (char c : charArray) {
+            sum += c - '0';
+        }
+        return sum % 9;
+    }
+}
