5884c2e20110ceadc05cbed9f1b34833d8cbb351
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-02 12:25:36 +0900

    solution10

---
+++ src/solutions/Solution10.java
@@ -0,0 +1,28 @@
+package solutions;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181913
+ */
+class Solution10 {
+    public String solution(String str, int[][] queries) {
+        StringBuilder sb = new StringBuilder(str);
+
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            reverse(start, end, sb);
+        }
+
+        return sb.toString();
+    }
+
+    public void reverse(int start, int end, StringBuilder sequence) {
+        while (start < end) {
+            char temp = sequence.charAt(start);
+            sequence.setCharAt(start, sequence.charAt(end));
+            sequence.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+    }
+}
