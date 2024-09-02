74b23b97272b9ccaddfcf60259ed669ab8b6ca74
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-02 20:54:13 +0900

    solution14

---
+++ src/solutions/Solution14.java
@@ -0,0 +1,19 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181909
+ */
+class Solution14 {
+    public String[] solution(String str) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.substring(i));
+        }
+        Collections.sort(list);
+        return list.toArray(new String[0]);
+    }
+}
