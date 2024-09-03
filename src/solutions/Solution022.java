8c998d0ed37d71fab51914d43adf3156c9f70692
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-03 09:05:12 +0900

    Solution022, Solution023

---
+++ src/solutions/Solution022.java
@@ -0,0 +1,14 @@
+package solutions;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181904
+ */
+class Solution022 {
+    public String solution(String str, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i += m) {
+            sb.append(str.charAt(i + c - 1));
+        }
+        return sb.toString();
+    }
+}
---
+++ src/solutions/Solution023.java
@@ -0,0 +1,16 @@
+package solutions;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181903
+ */
+class Solution023 {
+    public String solution(int q, int r, String code) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r) {
+                sb.append(code.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
