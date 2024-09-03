66dd11a8b177936ffa6e56f29561ea1d430c4d4a
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-04 00:07:04 +0900

    Solution033

---
+++ src/solutions/Solution033.java
@@ -0,0 +1,30 @@
+package solutions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181844
+ */
+class Solution033 {
+    public int[] solution(int[] arr, int[] deleteArr) {
+        Set<Integer> deleteSet = new HashSet<>();
+        for (int num : deleteArr) {
+            deleteSet.add(num);
+        }
+
+        int[] temp = new int[arr.length];
+        int index = 0;
+
+        for (int num : arr) {
+            if (!deleteSet.contains(num)) {
+                temp[index++] = num;
+            }
+        }
+
+        int[] result = new int[index];
+        System.arraycopy(temp, 0, result, 0, index);
+
+        return result;
+    }
+}
