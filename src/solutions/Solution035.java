8b1f785d55a0fe541fae0f5815619f63519f16ba
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-04 09:11:11 +0900

    Solution035

---
+++ src/solutions/Solution035.java
@@ -0,0 +1,22 @@
+package solutions;
+
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181895
+ */
+class Solution035 {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = intervals[0][0]; i <= intervals[0][1]; i++) {
+            result.add(arr[i]);
+        }
+
+        for (int i = intervals[1][0]; i <= intervals[1][1]; i++) {
+            result.add(arr[i]);
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
