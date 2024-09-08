a10d514a6b3e66069d6cdb218da6118238c7a56a
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-08 16:22:23 +0900

    Solution051

---
+++ src/solutions/level0/Solution051.java
@@ -0,0 +1,10 @@
+package solutions.level0;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181843
+ */
+class Solution051 {
+    public int solution(String input, String target) {
+        return input.contains(target) ? 1 : 0;
+    }
+}
