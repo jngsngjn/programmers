89956186dfad6c2aa013c981938612da08fe0c63
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-08 10:32:56 +0900

    Solution049

---
+++ src/solutions/level0/Solution049.java
@@ -0,0 +1,10 @@
+package solutions.level0;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181842
+ */
+class Solution049 {
+    public int solution(String str1, String str2) {
+        return str2.contains(str1) ? 1 : 0;
+    }
+}
