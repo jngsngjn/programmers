7891f3d757a41c2f186af90d5bf7762b5e4bd349
jngsngjn <wjdtjdwls98@gmail.com>
2024-09-11 00:15:11 +0900

    Solution089

---
+++ src/solutions/level0/Solution089.java
@@ -0,0 +1,29 @@
+package solutions.level0;
+
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181851
+ */
+class Solution089 {
+    public int solution(int[] rank, boolean[] attendance) {
+        Map<Integer, Integer> map = new HashMap<>(); // rank, index
+        for (int i = 0; i < rank.length; i++) {
+            map.put(rank[i], i);
+        }
+
+        Arrays.sort(rank);
+        int[] selected = new int[3];
+        int index = 0;
+        for (int i = 0; i < rank.length; i++) {
+            if (selected[2] != 0) {
+                break;
+            }
+
+            if (attendance[map.get(i + 1)]) {
+                selected[index++] = map.get(i + 1);
+            }
+        }
+        return (10000 * selected[0]) + (100 * selected[1]) + selected[2];
+    }
+}
